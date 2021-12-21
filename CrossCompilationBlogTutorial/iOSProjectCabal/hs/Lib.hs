f81a9e16624b9959efcdfe7bba7b02e1d9b8f864
ernestkz <ernestkuznecovs@gmail.com>
2021-12-21 11:55:53 +0000

    Sqlite task completion and Simplex compilation observations.

--- CrossCompilationBlogTutorial/iOSProjectCabal/hs/Lib.hs
+++ CrossCompilationBlogTutorial/iOSProjectCabal/hs/Lib.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib where
 
 import Foreign.C (CString, newCString)
-import Network.HTTP
+import Network.HTTP (simpleHTTP, getRequest, getResponseBody)
+import Database.SQLite3 (close, open)
 
 -- | export haskell function @chello@ as @hello@.
 -- foreign export ccall "hello" chello :: IO CString
@@ -9,6 +11,8 @@ foreign export ccall "hello" httpRequest :: IO CString
 
 httpRequest :: IO CString
 httpRequest = do
+  -- db <- open "file:data.db"
+  -- close db
   res <- simpleHTTP (getRequest "http://www.google.com/") >>= fmap (take 100) . getResponseBody
   newCString res
 
