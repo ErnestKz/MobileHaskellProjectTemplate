f4571275bc26e3f1276eaabc3d6796405fd4263c
ernestkz <ernestkuznecovs@gmail.com>
2021-12-27 09:37:37 +0000

    Setup new tasks.

---
+++ CrossCompilationBlogTutorial/shell.nix
@@ -0,0 +1,9 @@
+# Environment for cross compilers.
+# - Download and Setup paths.
+
+# Setup iserv proxy for build machine.
+# - Shell that has iserv proxy.
+
+# Provide expression to build GHCSlave
+# - Cross compile iserv.
+# - Cross compile GHCSlave linking against iserv.
