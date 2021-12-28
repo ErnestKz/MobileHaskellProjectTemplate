3e2e7139b7e157e2cfae68efff86ba779c586222
ernestkz <ernestkuznecovs@gmail.com>
2021-12-28 12:36:50 +0000

    Cabal wrapper with nix.

--- CrossCompilationBlogTutorial/toolchain.nix
+++ CrossCompilationBlogTutorial/toolchain.nix
@@ -28,15 +28,15 @@ rec {
       }) { });
   
   cabal-cross = (pkgs.callPackage
-    ({ pkgs, stdenv, fetchFromGitHub
+    ({ pkgs, stdenv, fetchFromGitHub, writeShellScriptBin, writeScript
      , cabal-install
      , haskellPackages
      , darwin
      , xcbuild
      , target ? "aarch64-apple-ios"
-     }: stdenv.mkDerivation {
-       name = "cabal-${target}";
-       cabalWrapper = let
+     }: stdenv.mkDerivation rec {
+       name = "${target}-cabal";
+       wrappedCabal = let
          xcrun = "${xcbuild}/bin/xcrun";
          clang = "${xcbuild}/Toolchains/XcodeDefault.xctoolchain/bin/clang";
          ld = "${xcbuild}/Toolchains/XcodeDefault.xctoolchain/bin/ld";
@@ -50,18 +50,19 @@ rec {
                                        clang = "";
                                        ld = "";
                                        hsc2hs = ""; }; };
-         deps2 = deps.${target};
-         # need to make each of the deps a binary
-       in ''
-       fcommon="--builddir=dist/${target}"
-       fcompile=" --with-ghc=${target}-ghc"
-       fcompile+=" --with-ghc-pkg=${target}-ghc-pkg"
-       fcompile+=" --with-gcc=${target}-clang"
-       fcompile+=" --with-ld=${target}-ld"
-       fcompile+=" --with-hsc2hs=${target}-hsc2hs"
-       fcompile+=" --hsc2hs-options=--cross-compile"
-	     fconfig="--disable-shared --configure-option=--host=${target}"
-       case $1 in
+         wrappedCommands' = builtins.mapAttrs (name: value: (writeShellScriptBin "${target}-${name}" value)) deps.${target};
+         wrappedCommands = builtins.mapAttrs (name: value: "${value}/bin/${target}-${name}") wrappedCommands';
+       in writeShellScriptBin name
+         ''
+         fcommon="--builddir=dist/${target}"
+         fcompile=" --with-ghc=${wrappedCommands.ghc}"
+         fcompile+=" --with-ghc-pkg=${wrappedCommands.ghc-pkg}"
+         fcompile+=" --with-gcc=${wrappedCommands.clang}"
+         fcompile+=" --with-ld=${wrappedCommands.ld}"
+         fcompile+=" --with-hsc2hs=${wrappedCommands.hsc2hs}"
+         fcompile+=" --hsc2hs-options=--cross-compile"
+         fconfig="--disable-shared --configure-option=--host=${target}"
+            case $1 in
 	          configure|install) flags="''${fcommon} ''${fcompile} ''${fconfig}" ;;
 	          build)             flags="''${fcommon} ''${fcompile}" ;;
 	          new-configure|new-install) flags="''${fcompile} ''${fconfig}" ;;
@@ -69,19 +70,20 @@ rec {
 	          list|info|update)  flags="" ;;
 	          "")                flags="" ;;
 	          *)                 flags=$fcommon ;;
-       esac;;
-
-       ${deps2.ghc}
-       ${darwin.xcode}
-       ${xcbuild}
-       '';
-       
-       builder = ./nixScripts/init_toolchain.sh;
+            esac
+         exec ${cabal-install}/bin/cabal $flags $@
+         '';
+       builder = writeScript "builder"
+         ''
+          source $stdenv/setup
+          cp ${wrappedCabal}/bin/${name} $out
+         '';
        buildInputs = [
           pkgs.llvmPackages_5.llvm # looks like does it does setup includes as well
           ghc-aarch64-apple-ios
           ghc-x86_64-apple-ios
           darwin.xcode
+          # wrappedCabal
        ];
       }) { });
 }
